e92d299af07da3fb65e7917c1e14cc061ee0df50
Aishwar <aishwar.16218@gmail.com>
2026-08-08 00:59:11 +0530

    GODDDD

--- server/utils/kosonClient.js
+++ server/utils/kosonClient.js
@@ -2,20 +2,20 @@ import axios from 'axios';
 
 class KosonClient {
   constructor() {
-    this.defaultUrl = 'https://api.kosontechnology.com/india-hotel.php';
+    this.defaultUrl = KOSON_HOTEL_API_URL;
   }
 
   async getHotels() {
     try {
       const url = process.env.KOSON_HOTEL_API_URL || this.defaultUrl;
       const response = await axios.get(url);
-      
+
       // If the API returns a string, try to parse it, otherwise return as is
       let data = response.data;
       if (typeof data === 'string') {
         data = JSON.parse(data);
       }
-      
+
       return data;
     } catch (error) {
       console.error('Koson API Error:', error.message);
